471d5b9b4784c8a0ff5466ecc468cc55c7d88653
worlddeleteRin <worlddelete0@gmail.com>
2022-03-01 10:32:01 +0300

    bot tasks models

--- main_app/apps/bots_tasks/models.py
+++ main_app/apps/bots_tasks/models.py
@@ -3,7 +3,7 @@ from enum import Enum, unique
 from datetime import datetime
 from apps.site.utils import get_time_now
 from pymongo.results import InsertOneResult
-from pydantic import BaseModel, Field, UUID4
+from pydantic import BaseModel, Field, UUID4, validator, ValidationError, root_validator
 from typing import Optional, Union
 
 from database.main_db import db_provider
@@ -11,12 +11,99 @@ from pymongo import ReturnDocument
 
 from apps.bots.models import PlatformEnum
 
+class TaskTypeEnum(str, Enum):
+    like_post = 'like_post'
+    repost_post = 'repost_post'
+    dummy = 'dummy'
+
+class FrequencyTypeEnum(str, Enum):
+    once = 'once'
+    periodicly = 'periodicly'
+
+class WorkLagEnum(str, Enum):
+    immediately = 'Immediately'
+    one_minute = '1 minute'
+    five_minutes = '5 minutes'
+    ten_minutes = '10 minutes'
+    thirty_minutes = '30 minutes'
+    one_hour = '1 hour'
+    three_hours = '3 hours'
+    one_day = '1 day'
+    two_days = '2 days'
+    three_days = '3 days'
+    one_week = '1 week'
+    one_month = 'one month' 
+    custom_date = 'Custom date'
+
+class TimeFrequency(BaseModel):
+    pass
+
+class TaskDateFinish(BaseModel):
+    date: datetime
+
+class LikePostTargetData(BaseModel):
+    post_link: str
+    like_count: int
+    work_lag: WorkLagEnum = WorkLagEnum.one_minute
+    date_finish: Optional[TaskDateFinish] = None
+
+    @root_validator
+    def validate_like_post_target_data(cls, values):
+        work_lag = values.get('work_lag')
+        date_finish = values.get('date_finish')
+        print('values are', values)
+
+        if not isinstance(work_lag, WorkLagEnum):
+            raise ValueError('incorrect work_lag value')
+        if work_lag == WorkLagEnum.custom_date:
+            if not isinstance(date_finish, TaskDateFinish):
+                raise ValueError('provide date_finish')
+        return values
+        
+
+class TaskTargetData(BaseModel):
+    """
+        Task target data model
+    """
+    like_post: Optional[LikePostTargetData] = None
+
+class TaskType(BaseModel):
+    """
+        Task type model
+    """
+    id: TaskTypeEnum
+    name: str
+    platforms: list[PlatformEnum]
+
 class CreateBotTask(BaseModel):
     """
         Base create bot task
     """
     title: str = ""
     platform: PlatformEnum
+    task_type: TaskTypeEnum
+    task_target_data: TaskTargetData
+
+    @root_validator
+    def validate_create_bot(cls, values):
+        task_type = values.get('task_type')
+        task_target_data = values.get('task_target_data')
+        print('values are', values)
+
+        if not isinstance(task_type, TaskTypeEnum):
+            raise ValueError('provide correct task_type')
+        if not isinstance(task_target_data, TaskTargetData):
+            raise ValueError('provide correct task_target_data')
+
+        if task_type is TaskTypeEnum.like_post:
+            if not task_target_data.like_post:
+                raise ValueError('provide like_post')
+        """
+        if cls.task_type is TaskTypeEnum.repost_post:
+            if not cls.task_target_data.repost_post:
+                raise ValueError('provide repost_post')
+        """
+        return values
 
 
 class BotTask(BaseModel):
@@ -26,6 +113,8 @@ class BotTask(BaseModel):
     id: UUID4 = Field(default_factory=uuid.uuid4, alias="_id")
     title: str = ""
     platform: Optional[PlatformEnum] = None
+    task_type: TaskTypeEnum = TaskTypeEnum.dummy
+    task_target_data: TaskTargetData
 
     def save_db(self) -> InsertOneResult | None:
         inserted_bot: InsertOneResult = db_provider.bots_tasks_db.insert_one(self.dict(by_alias=True)
@@ -61,21 +150,26 @@ class BotTasksSearchQuery:
     skip: int = 0
     limit: int = 10
     platform: PlatformEnum | None
+    task_type: TaskTypeEnum | None
     def __init__(
         self,
         skip: int = 0,
         limit: int =10,
-        platform: PlatformEnum = None
+        platform: PlatformEnum = None,
+        task_type: TaskTypeEnum = None
     ):
         self.skip = skip
         self.limit = limit
         self.platform = platform
+        self.task_type = task_type
 
     def collect_db_filters_query(self) -> dict:
         filters = {}
 
         if (self.platform):
             filters['platform'] = self.platform
+        if (self.task_type):
+            filters['task_type'] = self.task_type
 
         return filters
 
