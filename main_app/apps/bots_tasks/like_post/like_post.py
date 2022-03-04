225467af3f2e6d66d9348407d949e9424ea3ae40
worlddeleteRin <worlddelete0@gmail.com>
2022-03-04 12:40:52 +0300

    add new features

--- main_app/apps/bots_tasks/like_post/like_post.py
+++ main_app/apps/bots_tasks/like_post/like_post.py
@@ -1,9 +1,15 @@
 from apps.bots.bots import get_bots
-from apps.bots.models import Bot, BotSearch, BotSearchQuery
+from apps.bots.models import Bot, BotSearch, BotSearchQuery, PlatformEnum
 from apps.bots_tasks.like_post.models import LikePostResultMetrics, LikePostTargetData
 from apps.bots_tasks.models import BotTask
 from apps.bots_tasks.utils import get_time_left_delimeter_from_timestamp
 
+def like_post_vk(
+    bots: list[Bot],
+    like_count: int,
+    bot_task: BotTask
+):
+    pass
 
 def process_like_post_task(
     bot_task: BotTask
@@ -37,7 +43,17 @@ def process_like_post_task(
     # get bots for task 
     bot_search: BotSearch = get_bots(bot_filter_query)
     bots: list[Bot] = bot_search.bots
+    # check if not bots stop task attach error
+    # 
     print('time delimeter is', time_delimeter)
     print('need process now:', process_now_count)
     print('bots for task are', bots)
+    print('bots len is ', len(bots))
     print('run process like post task')
+    # run task based on platform type
+    if bot_task.platform == PlatformEnum.vk:
+        like_post_vk(
+            bots = bots,
+            like_count =process_now_count,
+            bot_task = bot_task
+        )
