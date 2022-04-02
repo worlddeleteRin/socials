0d6225cd5d33949ec6042c12167cd01fa0741156
worlddeleteRin <worlddelete0@gmail.com>
2022-04-02 22:20:25 +0300

    add new features

--- main_app/apps/bots_tasks/like_post/like_post.py
+++ main_app/apps/bots_tasks/like_post/like_post.py
@@ -1,104 +1,12 @@
 from apps.bots.bots import get_bots
 from apps.bots.models import Bot, BotSearch, BotSearchQuery, BotSortByEnum, PlatformEnum
-from apps.bots_events.models import BotEvent
-from apps.bots_tasks.enums import TaskTypeEnum
+from apps.bots_tasks.like_post.like_post_vk import like_post_vk
+# from apps.bots_events.models import BotEvent
+# from apps.bots_tasks.enums import TaskTypeEnum
 from apps.bots_tasks.like_post.models import LikePostResultMetrics, LikePostTargetData
-from apps.bots_tasks.models import BotTask, BotTaskError
-from apps.bots_tasks.task_errors import NoBotsForTaskError, VkErrorGetWallPost, VkErrorPostUrl
+from apps.bots_tasks.models import BotTask
+from apps.bots_tasks.task_errors import NoBotsForTaskError
 from apps.bots_tasks.utils import calculate_next_time_run, get_time_left_delimeter_from_timestamp
-# from ..vk_core.client import VkClient
-from vk_core.client import VkClient
-from vk_core.likes.likes import AddLikeQuery, IsLikedQuery, LikeTargetEnum, Likes
-from vk_core.likes.models import IsLikedResponse
-from vk_core.wall.wall import VkPost, VkWall, WallPostGetByIdQuery
-
-def like_post_vk(
-    bots: list[Bot],
-    like_count: int,
-    metrics: LikePostResultMetrics,
-    bot_task: BotTask
-):
-    print('run like post vk')
-    # check if can get wall post
-    if not bot_task.task_target_data.like_post:
-        bot_task.setError(VkErrorGetWallPost)
-        return
-    link = bot_task.task_target_data.like_post.post_link
-    postUrlInfo = VkWall.getOwnerItemIdsFromUrl(
-        link
-    )
-    if not postUrlInfo:
-        bot_task.setError(VkErrorPostUrl)
-        return
-
-    defaultClient = VkClient(
-        access_token = bots[0].access_token
-    )
-    wall = VkWall(client = defaultClient, owner_id = int(postUrlInfo[0]))
-    postQuery = WallPostGetByIdQuery(
-        posts = f"{postUrlInfo[0]}_{postUrlInfo[1]}"
-    )
-    print('wall is', wall.__dict__)
-    print('post url info is', postUrlInfo)
-    try:
-        currentPost: VkPost = wall.getById(query = postQuery)[0]
-    except Exception as e:
-        print('error get post', e)
-        bot_task.setError(VkErrorGetWallPost)
-        return
-    isLikedQuery = IsLikedQuery(
-        type = LikeTargetEnum.post,
-        owner_id = wall.owner_id,
-        item_id = currentPost.id
-    )
-    likeQuery = AddLikeQuery(
-        type = LikeTargetEnum.post,
-        owner_id = wall.owner_id,
-        item_id = currentPost.id
-    )
-
-    print('post id kis', currentPost.id)
-    print('like Query is', likeQuery)
-    for bot in bots:
-        # set up client
-        client = VkClient(
-            access_token = bot.access_token
-        )
-        print('client is', client)
-        # check, if bot already like post
-        isLiked: IsLikedResponse = Likes(client = client).isLiked(
-            query = isLikedQuery
-        )
-        print('isLiked query is', isLiked)
-        if (isLiked.isLiked()):
-            bot_task.bots_used.append(bot.id)
-            continue
-
-        # try to like post
-        response = Likes(client = client).add(query = likeQuery)
-        print('response is', response)
-        # add bot id to used
-        try:
-            bot_task.bots_used.append(bot.id)
-            # add metrics TODO
-            metrics.like_count += 1
-            # add bot event TODO
-            event = BotEvent(
-                event_type = TaskTypeEnum.like_post, 
-                platform = PlatformEnum.vk,
-                bot_id = bot.id,
-                task_id = bot_task.id,
-                count_amount = 1
-            )
-            event.save_db()
-            # update bot metrics like_count
-            bot.daily_metrics.like_count += 1
-            bot.hourly_metrics.like_count += 1
-            # update bot last used
-            bot.update_db(update_used=True)
-        except Exception as e:
-            print('exception occured', e)
-            bot_task.setError(BotTaskError.dummy_error(e))
 
 def process_like_post_task(
     bot_task: BotTask
@@ -194,4 +102,3 @@ def process_like_post_task(
     # update bot db
     bot_task.update_db()
     # ?
-
